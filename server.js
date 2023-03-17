9848645e6a58217c61ef4f6c369c2e15b12b538a
raj2852 <microsoftrajdip@gmail.com>
2023-03-17 17:12:35 +0530

    database connected

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config.env' });
 
 connectDB();
 
